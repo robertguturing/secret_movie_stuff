2a6aaee5be68329c1ebe7c0f1d9f9aa4f16d82e8
Robert Gu <robertgu@Roberts-MacBook-Pro.local>
2020-04-23 08:55:47 -0600

    Configure database to use ENV variable

---
+++ app/controllers/MoviesController.rb
@@ -0,0 +1,10 @@
+class MoviesController < ApplicationController
+  def index
+    # return all the movies
+
+    # It looks like we established the connection
+    # Next goal is do we create the migrations by hand, but I think we should probably import something from the db to define our structure
+    # How do we actually get the info from the database we are using
+    # We don't have a model
+  end
+end
